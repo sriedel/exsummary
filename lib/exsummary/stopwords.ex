1304e9edd7040950eaefc02268aac4594dcc7097
Sven Riedel <sr@gimp.org>
2017-02-06 11:02:29 +0100

    Add stopword filtering of text

--- lib/exsummary/stopwords.ex
+++ lib/exsummary/stopwords.ex
@@ -1,6 +1,10 @@
 defmodule ExSummary.Stopwords do
   @stopwords "config/stopwords.txt" |> File.read! |> String.split
+  @stopword_re with {:ok, re } <- Regex.compile( "\\b\\s*(#{Enum.join( @stopwords, "|" )})\\s*\\b" ), do: re
   
   @spec list() :: [ binary ]
   def list, do: @stopwords
+
+  @spec filter( binary ) :: binary
+  def filter( text ), do: IO.inspect( @stopword_re ) && String.replace( text, @stopword_re, "" ) 
 end
--- spec/lib/exsummary/stopwords_spec.exs
+++ spec/lib/exsummary/stopwords_spec.exs
@@ -6,4 +6,12 @@ defmodule ExSummary.StopwordsSpec do
       expect ExSummary.Stopwords.list |> to( be_list )
     end
   end
+
+  describe ".filter" do
+    it "should remove all occurrances of the stopwords from the given text" do
+      raw_text = "i am on. it is nice. i am off."
+      expected_result = ". nice. ."
+      expect ExSummary.Stopwords.filter( raw_text ) |> to( eq expected_result )
+    end
+  end
 end
