e319e3ed04d1540fe8ae944d7682de8b55efcde0
Sven Riedel <sr@gimp.org>
2017-02-06 10:13:19 +0100

    Initial commit

---
+++ lib/exsummary.ex
@@ -0,0 +1,18 @@
+defmodule Exsummary do
+  @moduledoc """
+  Documentation for Exsummary.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exsummary.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exsummary_test.exs
@@ -0,0 +1,8 @@
+defmodule ExsummaryTest do
+  use ExUnit.Case
+  doctest Exsummary
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
