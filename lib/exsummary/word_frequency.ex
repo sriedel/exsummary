a9c4ad9c307f006ea3c7c4d70ba87c855d50f255
Sven Riedel <sr@gimp.org>
2017-02-06 12:44:33 +0100

    Refactor WordFrequency.histogram

--- lib/exsummary/word_frequency.ex
+++ lib/exsummary/word_frequency.ex
@@ -2,9 +2,20 @@ defmodule ExSummary.WordFrequency do
   @spec histogram( binary ) :: map
   def histogram( text ) do
     text
-    |> String.split
-    |> Enum.group_by( &( String.replace( &1, ~r/[^'\p{Xan}]/, "" ) ) )
-    |> Enum.map( fn( {key, value} ) -> { key, Enum.count( value ) } end )
-    |> Enum.into( %{} )
+    |> valid_word_list
+    |> Enum.reduce( %{}, fn( word, histogram ) ->
+      {_, histogram} = Map.get_and_update( histogram, word, &( { &1, (&1||0) + 1 } ) ) 
+      histogram
+    end )
+  end
+
+  defp is_valid_word?( word ) do
+    String.length( word ) > 0 && !Regex.match?( ~r/[^\p{Xan}]/, word )
+  end
+
+  defp valid_word_list( text ) do
+    text 
+    |> String.split( ~r/\s*(\b|[^\p{Xan}])\s*/ )
+    |> Enum.filter( &( is_valid_word?( &1 ) ) )
   end
 end
