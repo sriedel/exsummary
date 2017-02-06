df883b3cec15a96a950cd42eb4995b02966c83d3
Sven Riedel <sr@gimp.org>
2017-02-06 10:34:51 +0100

    Add common dependencies

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Exsummary.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     preferred_cli_env: [ espec: :test ],
+     test_coverage: [ tool: Coverex.Task ],
      deps: deps()]
   end
 
@@ -28,6 +30,10 @@ defmodule Exsummary.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [ {:espec, "1.2.1", only: :test},
+      {:credo, "~> 0.5", only: [ :test, :dev ] },
+      {:coverex, "~> 1.4.10", only: :test },
+      {:ex_doc, ">= 0.0.0", only: :dev }
+    ]
   end
 end
