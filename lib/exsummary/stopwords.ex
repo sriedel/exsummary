bea116a46df170a8833bde27d07838ba6076e56d
Sven Riedel <sr@gimp.org>
2017-02-08 11:46:29 +0100

    Fix stopword filtering to not replace word boundary characters

--- lib/exsummary/stopwords.ex
+++ lib/exsummary/stopwords.ex
@@ -1,10 +1,10 @@
 defmodule ExSummary.Stopwords do
   @stopwords "config/stopwords.txt" |> File.read! |> String.split
-  @stopword_re with {:ok, re } <- Regex.compile( "\\b\\s*(#{Enum.join( @stopwords, "|" )})\\s*\\b" ), do: re
+  @stopword_re with {:ok, re } <- Regex.compile( "(\\b\\s*)(?:#{Enum.join( @stopwords, "|" )})(\\s*\\b)" ), do: re
   
   @spec list() :: [ binary ]
   def list, do: @stopwords
 
   @spec filter( binary ) :: binary
-  def filter( text ), do: String.replace( text, @stopword_re, "" ) 
+  def filter( text ), do: Regex.replace( @stopword_re, text, "\\1\\2" ) |> String.replace( ~r/\s+/, " " ) |> String.trim
 end
