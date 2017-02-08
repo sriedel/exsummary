b93199d5ac2b45b979887a827c2d79aca8fbd352
Sven Riedel <sr@gimp.org>
2017-02-08 11:59:55 +0100

    First stub of summarize function

--- lib/exsummary.ex
+++ lib/exsummary.ex
@@ -1,18 +1,40 @@
-defmodule Exsummary do
+defmodule ExSummary do
   @moduledoc """
-  Documentation for Exsummary.
   """
 
-  @doc """
-  Hello world.
+  @spec summarize( binary ) :: binary
+  def summarize( text ) do
+    full_sentences = ExSummary.Sentence.split_into_sentences( text )
 
-  ## Examples
+    stemmed_sentences = full_sentences
+                        |> Enum.map( &ExSummary.Stopwords.filter/1 )
+                        |> Enum.map( &stem_text/1 )
 
-      iex> Exsummary.hello
-      :world
 
-  """
-  def hello do
-    :world
+    stemmed_text = stemmed_sentences
+                   |> Enum.join( " " )
+ 
+    word_scores = stemmed_text 
+                  |> ExSummary.WordFrequency.histogram
+                  |> ExSummary.Scoring.word_score_map
+
+    sentence_scores = stemmed_sentences
+                      |> Enum.map( &String.split/1 )
+                      |> Enum.map( &( ExSummary.Scoring.word_list_score( word_scores, &1 ) ) )
+
+    sentence_scores_with_position = Enum.zip( [ 1..length( sentence_scores ), sentence_scores, full_sentences ] )
+    sentence_scores_with_position 
+    |> Enum.sort_by( fn( { _pos, score, _sentence } ) -> score end, &>=/2 )
+    |> Enum.take( 2 )
+    |> Enum.sort_by( fn( { pos, _score, _sentence } ) -> pos end )
+    |> Enum.map( fn( { _pos, _score, sentence } ) -> sentence end )
+    |> Enum.join( " " )
+  end
+
+  defp stem_text( text ) do
+    text
+    |> String.split
+    |> Enum.map( &Porter2.stem/1 )
+    |> Enum.join( " " )
   end
 end
