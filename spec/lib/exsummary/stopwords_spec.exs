c4b9ad47b35a79e3c424aa01dc7ba15cfd7397a9
Sven Riedel <sr@gimp.org>
2017-02-09 09:05:53 +0100

    Fix variable vs function warning

--- spec/lib/exsummary/stopwords_spec.exs
+++ spec/lib/exsummary/stopwords_spec.exs
@@ -3,7 +3,7 @@ defmodule ExSummary.StopwordsSpec do
 
   describe ".list" do
     it "should return a list of the defined stopwords" do
-      expect ExSummary.Stopwords.list |> to( be_list )
+      expect ExSummary.Stopwords.list |> to( be_list() )
     end
   end
 
