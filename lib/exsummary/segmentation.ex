9b0a1ef9dff357e03cb64ab2f8d9f721f161b05d
Sven Riedel <sr@gimp.org>
2017-02-10 09:07:28 +0100

    Move sentence segmentation logic into ExSummary.Segmentation.sentence_segmentation

--- lib/exsummary/segmentation.ex
+++ lib/exsummary/segmentation.ex
@@ -1,4 +1,38 @@
 defmodule ExSummary.Segmentation do
+  @known_abbreviations MapSet.new( [ "us" ] )
+
+  @doc """
+    Segments the given text into a list of sentences.
+  """
+  @spec sentence_segmentation( binary ) :: [ binary ]
+  def sentence_segmentation( text ) do
+    ~r/(\S+)[.?!]\s+\p{Lu}/u
+    |> Regex.scan( text, return: :index )
+    |> Enum.reduce( [], fn( [ _, { prev_token_index, prev_token_length } ], acc ) ->
+      prev_token = binary_part( text, prev_token_index, prev_token_length )
+
+      [ { prev_token_index + prev_token_length + 1, prev_token } | acc ]
+    end )
+    |> Enum.filter( fn( { _index, token } ) -> !known_abbreviation?( token ) end )
+    |> Enum.map( fn( { index, _token } ) -> index end ) 
+    |> split_text_on_bytes( text, [] )
+    |> Enum.map( &String.trim/1 )
+  end
+  defp split_text_on_bytes( [], text, acc ), do: [ text | acc ]
+  defp split_text_on_bytes( [ index | rest ], text, acc ) do
+    substring_length = byte_size( text ) - index
+    split_text_on_bytes( rest, 
+                         binary_part( text, 0, index ),
+                         [ binary_part( text, index, substring_length ) | acc ] ) 
+  end
+
+  defp known_abbreviation?( word ) do
+    normalized_word = word
+                      |> String.replace( ~r/\W/, "" ) 
+                      |> String.downcase
+    MapSet.member?( @known_abbreviations, normalized_word )
+  end
+
   @doc """
     Segments the given text into a list of words.
   """
--- spec/lib/exsummary/segmentation_spec.exs
+++ spec/lib/exsummary/segmentation_spec.exs
@@ -1,6 +1,93 @@
 defmodule ExSummary.SegmentationSpec do
   use ESpec
 
+  describe ".sentence_segmentation" do
+    context "if the text has no punctuation" do
+      let :text, do: "this is a text without punctuation"
+
+      it "should return a list of one element containing the input text" do
+        expect ExSummary.Segmentation.sentence_segmentation( text() ) |> to( eq [ text() ] )
+      end
+    end
+
+    context "if the text only has a period at its end" do
+      let :text, do: "this is a text with a period at the end."
+
+      it "should return a list of one element containing the input text" do
+        expect ExSummary.Segmentation.sentence_segmentation( text() ) |> to( eq [ text() ] )
+      end
+    end
+
+    context "if the text only has an exclamation mark at its end" do
+      let :text, do: "this is a text with an exclamation mark at the end!"
+
+      it "should return a list of one element containing the input text" do
+        expect ExSummary.Segmentation.sentence_segmentation( text() ) |> to( eq [ text() ] )
+      end
+    end
+
+    context "if the text only has a question mark at its end" do
+      let :text, do: "is this a text with a question mark at the end?"
+
+      it "should return a list of one element containing the input text" do
+        expect ExSummary.Segmentation.sentence_segmentation( text() ) |> to( eq [ text() ] )
+      end
+    end
+
+    context "if the text has multiple punctuation marks interspersed" do
+      context "and before an end-of-sentence-mark is a known abbreviation" do
+        let :text, do: "The Trump administration argued the president has broad authority to decide who can and can’t enter the U.S. and that preventing him from doing so puts national security at risk, as it sought to persuade an appeals court to reinstate a travel ban."
+
+        it "should not be counted as an end-of-sentence" do
+          expect ExSummary.Segmentation.sentence_segmentation( text() ) |> to( eq [ text() ] )
+        end
+      end
+
+      context "and before an end-of-sentence-mark is no known abbreviation" do
+        context "and the following token is capitalized" do
+          let :sentence1, do: "The Trump administration argued the president has broad authority to decide who can and can’t enter the here."
+          let :sentence2, do: "This puts national security at risk, as it sought to persuade an appeals court to reinstate a travel ban."
+          let :text, do: Enum.join( [ sentence1(), sentence2() ], " " )
+
+          it "should be counted as an end-of-sentence" do
+            expect ExSummary.Segmentation.sentence_segmentation( text() ) |> to( eq [ sentence1(), sentence2() ] )
+          end
+        end
+
+        context "and the following token is not capitalized" do
+          let :text, do: "The Trump administration argued the president has broad authority to decide who can and can’t enter the ... and that preventing him from doing so puts national security at risk, as it sought to persuade an appeals court to reinstate a travel ban."
+
+          it "should not be counted as an end-of-sentence" do
+            expect ExSummary.Segmentation.sentence_segmentation( text() ) |> to( eq [ text() ] )
+          end
+        end
+      end
+    end
+
+    it "can separate one sentence appropriately" do
+      sentence = "I am foo."
+      expect ExSummary.Segmentation.sentence_segmentation( sentence ) |> to( eq [ sentence ] )
+    end
+
+    it "can separate two sentences appropriately" do
+      sentences = [ "I am foo.", "I am bar." ]
+      text = Enum.join( sentences, " " )
+      expect ExSummary.Segmentation.sentence_segmentation( text ) |> to( eq sentences )
+    end
+
+    it "can separate three sentences appropriately" do
+      sentences = [ "I am foo.", "I am bar.", "I am baz." ]
+      text = Enum.join( sentences, " " )
+      expect ExSummary.Segmentation.sentence_segmentation( text ) |> to( eq sentences )
+    end
+
+    it "can separate four (and more) sentences appropriately" do
+      sentences = [ "I am foo.", "I am bar.", "I am baz.", "I am quux." ]
+      text = Enum.join( sentences, " " )
+      expect ExSummary.Segmentation.sentence_segmentation( text ) |> to( eq sentences )
+    end
+  end
+
   describe ".word_segmentation" do
     context "it should split words on word boundaries and remove leading and training space" do
       expect ExSummary.Segmentation.word_segmentation( "   foo Bar (baz)  quux  " ) |> to( eq [ "foo", "Bar", "(baz)", "quux" ] )
